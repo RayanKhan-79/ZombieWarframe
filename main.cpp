8e098c859217eb0a02ea5f969ab10b8005041ae0
Rayan Khan <rayankhannexus@gmail.com>
2024-04-26 11:50:53 +0500

    Resized window + zombie movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-﻿#include <SFML/Graphics.hpp>
+﻿#include<iostream>
+#include <SFML/Graphics.hpp>
 #include <ctime>
 //#include"../SFML/Images/"
 
@@ -9,8 +10,43 @@ using namespace std;
 struct coordinats {
 	int x;
 	int y;
+	coordinats(int x, int y) : x(x), y(y) {}
 };
 
+
+class Zombie
+{
+	int health;
+	int speed;
+	int damage;
+	Image image;
+	Texture texture;
+	Sprite sproota;
+	coordinats Pos;
+
+public:
+	Zombie(int health, int speed, int damage, int x, int y) :Pos(x,y), health(health), speed(speed), damage(damage)
+	{
+		image.loadFromFile("./Images/zombie.png");
+		texture.loadFromImage(image);
+		sproota.setTexture(texture);
+		sproota.setPosition(Pos.x, Pos.y);
+	}
+
+
+	void Draw(RenderWindow& window)
+	{
+		window.draw(sproota);
+	}
+
+	void Move()
+	{
+		Pos.x -= speed;
+		sproota.setPosition(Pos.x, Pos.y);
+	}
+
+
+};
 
 
 //Drawing the background
@@ -35,7 +71,7 @@ void createMap(RenderWindow& window) {
 	map.loadFromImage(map_image);
 	Sprite s_map;
 	s_map.setTexture(map);
-	s_map.setPosition(300, 160);
+	s_map.setPosition(300, 140);
 
 	window.draw(s_map);
 }
@@ -71,12 +107,27 @@ int main()
 
 	Clock timeMoney;
 	
+	Zombie z1(200, 1, 20, 1200, 150);
+	Zombie z2(200, 1, 20, 1200, 260);
+	Zombie z3(200, 2, 20, 1200, 370);
+	Zombie z4(200, 2, 20, 1200, 480);
+	Zombie z5(200, 3, 20, 1200, 590);
+
+
+
 
 
 	Clock clock;
-
+	Clock time;
+	double S_time = time.getElapsedTime().asSeconds();
+	window.setFramerateLimit(60);
 	while (window.isOpen())
 	{
+		double E_time = time.getElapsedTime().asSeconds();
+		double FPS = 1/(E_time);
+		time.restart();
+		cout << "FPS : " << FPS << endl;
+
 		float time = clock.getElapsedTime().asMicroseconds();
 		float moneyTime = timeMoney.getElapsedTime().asSeconds();
 
@@ -90,14 +141,26 @@ int main()
 				window.close();
 		}
 
+
+
 		//Create a background
 		createBack(window);
 		createMap(window);
 	
+		z1.Draw(window);
+		z1.Move();
+		z2.Draw(window);
+		z2.Move();
+		z3.Draw(window);
+		z3.Move();
+		z4.Draw(window);
+		z4.Move();
+		z5.Draw(window);
+		z5.Move();
 
 		
-
-		window.setSize(sf::Vector2u(550, 340));
+		window.setSize(sf::Vector2u(1200, 700));
+		window.setPosition(Vector2i(100, 100));
 		window.display();
 	}
 	return 0;

1a1e7873d7765a15cf7165a6f8bcfafdeeafcf50
Rayan Khan <rayankhannexus@gmail.com>
2024-05-07 23:06:44 +0500

    Added remaing seed Packet icons

--- PlantFactory.cpp
+++ PlantFactory.cpp
@@ -2,13 +2,13 @@
 PlantFactory::PlantFactory() : numPlants(0) , plantsUnlocked(8)
 {
     // Initialize all textures (even locked ones)
-    seedTextures[0].loadFromFile("./Images/img3.png");  // Sunflower
-    seedTextures[1].loadFromFile("./Images/img4.png");  // Peashooter
-    seedTextures[2].loadFromFile("./Images/img5.png");  // whaterver is unlocked in the next level
-    seedTextures[3].loadFromFile("./Images/img3.png");  // Sunflower
-    seedTextures[4].loadFromFile("./Images/img4.png");  // Peashooter
-    seedTextures[5].loadFromFile("./Images/img5.png");  // whaterver is unlocked in the next level
-    seedTextures[6].loadFromFile("./Images/img3.png");  // Sunflower
+    seedTextures[0].loadFromFile("./Images/sunFlower_i.png");  // Sunflower
+    seedTextures[1].loadFromFile("./Images/peaShooter_i.png");  // Peashooter
+    seedTextures[2].loadFromFile("./Images/wallNut_i.png");  // whaterver is unlocked in the next level
+    seedTextures[3].loadFromFile("./Images/cherryBomb_i.png");  // Peashooter
+    seedTextures[4].loadFromFile("./Images/rePeater_i.png");  // Sunflower
+    seedTextures[5].loadFromFile("./Images/snowPea_i.png");  // whaterver is unlocked in the next level
+    seedTextures[6].loadFromFile("./Images/fumeShroom_i.png");  // Sunflower
    
 
 
@@ -16,7 +16,7 @@ PlantFactory::PlantFactory() : numPlants(0) , plantsUnlocked(8)
 
     for (int i = 0; i < plantsUnlocked; i++)
     {
-        seedPackets[i] = new SeedPackets(coordinates(10, (80 * i) + 80), true);
+        seedPackets[i] = new SeedPackets(coordinates(10, (85 * i) + 50), true);
         seedPackets[i]->setTexture(seedTextures[i]);
     }
 }

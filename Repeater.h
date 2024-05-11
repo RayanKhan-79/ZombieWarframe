a82e53f454d37a1923558f2d6c574f623abbeac9
monizaheer1@gmail.com <monizaheer1@gmail.com>
2024-05-11 19:01:10 +0500

    Generic update

--- Repeater.h
+++ Repeater.h
@@ -2,5 +2,8 @@
 #include "ShootingPlants.h"
 class Repeater : public ShootingPlants
 {
+	Repeater() { }
+	Repeater(int x, int y, int health);
+	void UpdateAnimation(float interval);
 };
 
--- SnowPea.h
+++ SnowPea.h
@@ -5,5 +5,6 @@ class SnowPea : public ShootingPlants
 public:
 	SnowPea();
 	SnowPea(int x, int y, int health);
+	void UpdateAnimation(float interval);
 };
 
